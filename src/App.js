b78771a3befd97f98df75e3d7f8bb383ea51bf2f
Gowtam Paul <gowtam.p@iverbinden.com>
2023-10-07 15:02:11 +0530

    bootstrap added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavbarMain from "./NavbarMain";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <NavbarMain />
+    </>
+  );
+}
+
+export default App;
